82314eafb9f15fa6cfbdea69d9889eb682198cfe
piotrkardys <piotrk@mobilewebpro.pl>
2016-05-04 09:13:56 +0200

    Lesson2: creating the simple votes counter (opinion poll)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ﻿"REDUCE DATA WITH JAVASCRIPT ARRAY#REDUCE" 
 "29.04.2016 - 16:25 - start of the project" 
 "29.04.2016 - 16:50 - Lesson1: configuration of the project &familiarizing with the reduce method of the array; create files: index.html, main.js, webpack.config.js" 
+"04.05.2016 - 09:05 – Lesson2: familiarizing with the reduce method by creating the simple votes counter (opinion poll), update file main.js" 
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 /* LESSON 1 */ /* ======================================================================================================
-*/
 var reducer = function(accumulator, item) {			//it runs for each element in the array (for each item), accumulator is
 	return accumulator + item;						//the previous value (i.e. for the first time it is the initialValue)
 };
@@ -17,5 +16,34 @@ console.log("The sum2 is: " + total2);				//The sum is: 10
 var data3 = [1, 3, 4];
 var initialValue3 = 0;
 var total3 = data3.reduce(reducer, initialValue3);
-console.log("The sum3 is: " + total3);				//The sum is: 8
+console.log("The sum3 is: " + total3);				//The sum is: 0
+//----------------------------------------------------------------------------------------------------------------------
+*/
+
+/* LESSON 2 */ /* ======================================================================================================
+*/
+var votes = [					//representation of opinion poll "which framework is the best"
+  "angular",
+  "angular",
+  "react",
+  "react",
+  "react",
+  "angular",
+  "ember",
+  "react",
+  "vanilla"
+];
+
+var initialValue = {};			//initialValue to reduce the array properly
+
+var reducer = function(tally, vote) {
+	if (!tally[vote]) tally[vote] = 1;		//if there isn't field called for example 'angular' create it 
+	else tally[vote]++;						//otherwise just increment it
+
+	return tally;							//returns value of the accumulator
+};
+
+var result = votes.reduce(reducer, initialValue);
+
+console.log(result);
 //----------------------------------------------------------------------------------------------------------------------
